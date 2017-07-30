c3aeba01277cf17ea6dc89f9b21419249595af41
xxc <x007007007@hotmail.com>
2017-07-30 10:53:53 +0800

    delete old file

--- src/biblishelf_album/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BiblishelfAlbumConfig(AppConfig):
-    name = 'biblishelf_album'
--- src/biblishelf_vdisk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BiblishelfVdiskConfig(AppConfig):
-    name = 'biblishelf_vdisk'
