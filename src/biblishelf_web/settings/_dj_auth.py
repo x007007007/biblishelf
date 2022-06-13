94d350ca0c868a821e6c90a28e2e72ee2aacff3d
xxc <x007007007@hotmail.com>
2022-06-13 11:12:31 +0800

    bug fix

--- src/biblishelf_web/settings/_dj_auth.py
+++ src/biblishelf_web/settings/_dj_auth.py
@@ -1,4 +1,4 @@
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # this is default
-    'guardian.backends.ObjectPermissionBackend',
+    # 'guardian.backends.ObjectPermissionBackend',
 )
